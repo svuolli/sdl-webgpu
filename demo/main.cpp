0393dfdb25e27f1224da650cb96242b06a1d3c52
Sami Vuolli <sami.vuolli@gmail.com>
2023-07-23 16:40:34 +0300

    Add basis for frame_renderer class

--- demo/main.cpp
+++ demo/main.cpp
@@ -45,7 +45,8 @@ struct wgpu_app
             throw std::runtime_error{"SDL_Webgpu_CreateSurface failed"};
         }
 
-        WGPURequestAdapterOptions const adapter_options = {
+        WGPURequestAdapterOptions const adapter_options =
+        {
             .nextInChain = nullptr,
             .compatibleSurface = wgpu_surface,
             .powerPreference = WGPUPowerPreference_Undefined,
@@ -61,7 +62,8 @@ struct wgpu_app
             throw std::runtime_error{"wgpuInstanceRequestAdapter failed"};
         }
 
-        WGPUDeviceDescriptor const device_descriptor = {
+        WGPUDeviceDescriptor const device_descriptor =
+        {
             .nextInChain = nullptr,
             .label = "Device",
             .requiredFeaturesCount = 0u,
@@ -80,6 +82,8 @@ struct wgpu_app
         }
 
         wgpuDeviceSetDeviceLostCallback(wgpu_device, nullptr, nullptr);
+        wgpuDeviceSetUncapturedErrorCallback(
+            wgpu_device, &wgpu_app::wgpu_error_callback, this);
 
         wgpu_queue = wgpuDeviceGetQueue(wgpu_device);
 
@@ -88,7 +92,8 @@ struct wgpu_app
             throw std::runtime_error{"WGPU Device has no command queue"};
         }
 
-        WGPUSwapChainDescriptor const swap_chain_descriptor = {
+        WGPUSwapChainDescriptor const swap_chain_descriptor =
+        {
             .nextInChain = nullptr,
             .label = "SwapChain",
             .usage = WGPUTextureUsage_RenderAttachment,
@@ -119,8 +124,14 @@ struct wgpu_app
     wgpu_app & operator=(wgpu_app const &) = delete;
     wgpu_app & operator=(wgpu_app &&) = delete;
 
-    static constexpr int width = 800;
-    static constexpr int height = 600;
+    static void wgpu_error_callback(
+        WGPUErrorType type, char const * message, void * user_data)
+    {
+        (void)user_data;
+        std::cerr <<
+            "Uncaught WGPU error(" << static_cast<int>(type) << "): " <<
+            message << std::endl;
+    }
 
     static WGPUAdapter requestAdapter(
         WGPUInstance instance, WGPURequestAdapterOptions const & opts)
@@ -205,6 +216,9 @@ struct wgpu_app
         return user_data.device;
     }
 
+    static constexpr int width = 800;
+    static constexpr int height = 600;
+
     WGPUInstance wgpu_instance = nullptr;
     SDL_Window * sdl_window = nullptr;
     WGPUAdapter wgpu_adapter = nullptr;
@@ -214,13 +228,91 @@ struct wgpu_app
     WGPUSwapChain wgpu_swap_chain = nullptr;
 };
 
+struct frame_renderer
+{
+    frame_renderer(wgpu_app & app_instance) :
+        app(app_instance)
+    {}
+
+    ~frame_renderer()
+    {}
+
+    void render(WGPUTextureView next_texture)
+    {
+        WGPUCommandEncoder encoder =
+            wgpuDeviceCreateCommandEncoder(app.wgpu_device, &command_encoder_descriptor);
+
+        WGPURenderPassEncoder render_pass = createRenderPassEncoder(encoder, next_texture);
+
+        // TODO: Add render commands
+
+        wgpuRenderPassEncoderEnd(render_pass);
+        WGPUCommandBuffer command_buffer =
+            wgpuCommandEncoderFinish(encoder, &command_buffer_descriptor);
+
+        wgpuQueueSubmit(app.wgpu_queue, 1, &command_buffer);
+
+        wgpuCommandBufferRelease(command_buffer);
+        wgpuRenderPassEncoderRelease(render_pass);
+        wgpuCommandEncoderRelease(encoder);
+    }
+
+    WGPURenderPassEncoder createRenderPassEncoder(
+        WGPUCommandEncoder encoder, WGPUTextureView target_view)
+    {
+        WGPURenderPassColorAttachment const color_attachment =
+        {
+            .nextInChain = nullptr,
+            .view = target_view,
+            .resolveTarget = nullptr,
+            .loadOp = WGPULoadOp_Clear,
+            .storeOp = WGPUStoreOp_Store,
+            .clearValue = bg_color
+        };
+
+        WGPURenderPassDescriptor const render_pass_descriptor =
+        {
+            .nextInChain = nullptr,
+            .label = "RenderPass",
+            .colorAttachmentCount = 1,
+            .colorAttachments = &color_attachment,
+            .depthStencilAttachment = nullptr,
+            .occlusionQuerySet = nullptr,
+            .timestampWriteCount = 0,
+            .timestampWrites = nullptr
+        };
+
+        return wgpuCommandEncoderBeginRenderPass(encoder, &render_pass_descriptor);
+    }
+
+    static constexpr WGPUCommandEncoderDescriptor command_encoder_descriptor =
+    {
+        .nextInChain = nullptr,
+        .label = "CommandEncoder"
+    };
+
+    static constexpr WGPUCommandBufferDescriptor command_buffer_descriptor =
+    {
+        .nextInChain = nullptr,
+        .label = "CommandBuffer"
+    };
+
+    static constexpr WGPUColor bg_color = {0.8, 0.2, 0.6, 1.0};
+
+    wgpu_app & app;
+};
+
 int main(int argc, char const * argv[])
 {
     try
     {
         wgpu_app app;
+        frame_renderer renderer(app);
 
         auto done = false;
+        auto const begin_time = SDL_GetTicks();
+        auto prev_time = begin_time;
+        auto frame_count = std::size_t{0};
 
         while(!done)
         {
@@ -232,7 +324,35 @@ int main(int argc, char const * argv[])
                     done = true;
                 }
             }
+
+            auto const current_time = SDL_GetTicks();
+            auto const delta_time = current_time - prev_time;
+            auto const elapsed_time = current_time - begin_time;
+
+            WGPUTextureView next_texture =
+                wgpuSwapChainGetCurrentTextureView(app.wgpu_swap_chain);
+
+            if(!next_texture)
+            {
+                std::cerr <<
+                    "Retrieving next texture view from swap chain failed\n";
+                continue;
+            }
+
+            renderer.render(next_texture);
+
+            wgpuTextureViewRelease(next_texture);
+            wgpuSwapChainPresent(app.wgpu_swap_chain);
+
+            prev_time = current_time;
+            ++frame_count;
         }
+        auto const elapsed_time = prev_time - begin_time;
+        std::cout << frame_count << " frames in " << elapsed_time << "ms\n";
+        auto const frame_rate =
+            1000.0 * static_cast<double>(frame_count) /
+            static_cast<double>(elapsed_time);
+        std::cout << frame_rate << "Hz\n";
 
     }
     catch (std::exception const & e)
