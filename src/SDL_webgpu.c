3a3a76cda347d38ede283243209e21678db15150
Sami Vuolli <sami.vuolli@gmail.com>
2023-07-30 17:45:55 +0300

    Add MacOS Metal support

--- src/SDL_webgpu.c
+++ src/SDL_webgpu.c
@@ -1,6 +1,33 @@
 #include "SDL_webgpu.h"
 #include <SDL2/SDL_syswm.h>
 
+#if defined(SDL_VIDEO_DRIVER_COCOA)
+#include <Foundation/Foundation.h>
+#include <QuartzCore/CAMetalLayer.h>
+#include <AppKit/NSWindow.h>
+
+static WGPUSurface SDL_Webgpu_CreateSurface_Cocoa(
+    SDL_SysWMinfo * wminfo, WGPUInstance instance)
+{
+    id metal_layer = [CAMetalLayer layer];
+    NSWindow * window = wminfo->info.cocoa.window;
+    [window.contentView setWantsLayer: YES];
+    [window.contentView setLayer: metal_layer];
+
+    WGPUSurfaceDescriptorFromMetalLayer const metal_surface_descriptor = {
+        .chain = { .next = NULL, .sType = WGPUSType_SurfaceDescriptorFromMetalLayer },
+        .layer = metal_layer,
+    };
+
+    WGPUSurfaceDescriptor const surface_descriptor = {
+        .label = NULL,
+        .nextInChain = &metal_surface_descriptor.chain,
+    };
+
+    return wgpuInstanceCreateSurface(instance, &surface_descriptor);
+}
+#endif
+
 #if defined(SDL_VIDEO_DRIVER_X11)
 static WGPUSurface SDL_Webgpu_CreateSurface_X11(
     SDL_SysWMinfo * wminfo, WGPUInstance instance)
@@ -57,6 +84,11 @@ WGPUSurface SDL_Webgpu_CreateSurface(
 #if defined(SDL_VIDEO_DRIVER_X11)
         case SDL_SYSWM_WAYLAND:
             return SDL_Webgpu_CreateSurface_Wayland(&info, instance);
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_COCOA)
+        case SDL_SYSWM_COCOA:
+            return SDL_Webgpu_CreateSurface_Cocoa(&info, instance);
 #endif
 
         default:
