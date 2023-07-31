5365f11673d0e3a7db23763ce32bac6ef59f6227
Sami Vuolli <sami.vuolli@gmail.com>
2023-07-31 17:22:36 +0300

    Use right SDL enum value for Windows

--- src/SDL_webgpu.c
+++ src/SDL_webgpu.c
@@ -112,7 +112,7 @@ WGPUSurface SDL_Webgpu_CreateSurface(
 #endif
 
 #if defined(SDL_VIDEO_DRIVER_WINDOWS)
-        case SDL_SYSWM_COCOA:
+        case SDL_SYSWM_WINDOWS:
             return SDL_Webgpu_CreateSurface_Win32(&info, instance);
 #endif
 
